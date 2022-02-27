942f70ad2511ce1fba9acd4c9171af4c3c9a6e8c
tom <22730962+ohitstom@users.noreply.github.com>
2022-02-27 04:26:40 +0000

    Fix

--- Comfy_Shadow.js
+++ Comfy_Shadow.js
@@ -4,26 +4,40 @@
     const main = document.querySelector('.Root__main-view')
     const LyricsBackground = document.querySelector('.lyrics-lyricsContainer-LyricsBackground')
 	const activityquery = document.querySelector("aside.main-buddyFeed-buddyFeedRoot")
-	const topbar = document.querySelector("header.main-topBar-container")
-    
-	if (!(Player && Menu && LocalStorage && Platform && main && topbar)) {
+    const topbar = document.querySelector("header.main-topBar-container")
+	
+    if (!(Player && Menu && LocalStorage && Platform && main && topbar)) {
         setTimeout(Comfy, 1000)
         return
     }
 	
-	topbar.style.paddingInlineEnd = "162px"
-	activityquery.addEventListener("mouseenter", e => {
-		topbar.style.paddingInlineEnd = "32px"
-	});
-	activityquery.addEventListener("mouseleave", e => {
-		topbar.style.paddingInlineEnd = "162px"
-	});
+	function computedStyleCondition(topbar, activityquery) {
+		if (getComputedStyle(activityquery).position == "relative") {
+			topbar.style.paddingInlineEnd = "32px"    
+		}
+		else {
+			topbar.style.paddingInlineEnd = "162px"    
+		}
+	}   
+
+    // Setting of topbar
+	!activityquery ? topbar.style.paddingInlineEnd = "162px" : (function(){computedStyleCondition(topbar, activityquery);
+
+	activityquery.addEventListener("mouseover", function( event ) { 
+	   computedStyleCondition(topbar, activityquery);
+	}, false);
+
+	activityquery.addEventListener("mouseout", function( event ) { 
+		computedStyleCondition(topbar, activityquery);
+	}, false);
 	
+	})(); 
+
+    // Spotify launching on a playlist		
     const channels = ['/playlist/', '/album/', '/collection/tracks', '/collection/episodes', '/episode/', '/lyrics-plus']
     main.appendChild(mainChild)
     mainChild.id = "mainImage"
 
-    // Spotify launching on a playlist
     for (var i = 0; i < channels.length; i++) {
         if (Platform.History.location.pathname.startsWith(channels[i])) {
             mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")"
